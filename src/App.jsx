03207aefdf770d8fb75e355e62ea59280a34ff9a
Ewa Szalwinska <e.szalwinska@gmail.com>
2025-11-09 22:25:58 -0800

    styling added to display all plant item and cart and cart items

--- src/App.jsx
+++ src/App.jsx
@@ -14,16 +14,14 @@ function PlantDetails({ weed, onAddToCart }) {
 }
 
 function Plants({ flowers, onAddToCart }) {
-  // const [chosenFlower, setChosenFlower] = useState({});
   return (
-    <section className="plant">
+    <section className="plants">
       <h2>Plants</h2>
       <ul>
         {flowers.map((flower) => (
           <PlantDetails
             key={flower.id}
             onAddToCart={onAddToCart}
-            // onClick={() => setChosenFlower(flower)}
             weed={flower}
           />
         ))}
@@ -76,48 +74,19 @@ export default function App() {
 
   function handleOnAddToCart(purchasedItem) {
     let newCart = [...cart];
-    if (newCart.length === 0) {
+    let found = newCart.find((item) => item === purchasedItem);
+    if (!found) {
       purchasedItem.quantity = 1;
       newCart.push(purchasedItem);
     } else {
-      let found = newCart.find((item) => item === purchasedItem);
-      if (!found) {
-        purchasedItem.quantity = 1;
-        newCart.push(purchasedItem);
-      } else {
-        newCart.forEach((item) => {
-          if (item.id === purchasedItem.id) {
-            item.quantity += 1;
-          }
-        });
-      }
+      newCart.forEach((item) => {
+        if (item.id === purchasedItem.id) {
+          item.quantity += 1;
+        }
+      });
     }
+
     setCart(newCart);
-
-    // newCart.forEach((item) => {
-    //   console.log("item94", item);
-    //   if (item.id === purchasedItem.id) {
-    //     console.log("item101", item);
-    //     item.quantity += 1;
-    //     console.log("item103", item);
-    //   } else {
-    //     purchasedItem.quantity = 1;
-    //     console.log("purchasedIte97", purchasedItem);
-    //     newCart.push(purchasedItem);
-    //     console.log("newCart99", newCart);
-    //   }
-    // });
-
-    // for (let plant of newCart) {
-    //   if ()
-    //   if (plant.id === purchasedItem.id) {
-    //     plant.quantity += 1;
-
-    //   }
-    // }
-    // purchasedItem.quantity = 1;
-    // newCart.push(purchasedItem);
-    // setCart(newCart);
   }
 
   function handleOnRemoveFromCart(purchasedItem) {
@@ -125,12 +94,6 @@ export default function App() {
     console.log("newCart", newCart);
     let filteredCart;
     let filteredItem = newCart.filter((item) => item.id === purchasedItem.id);
-    // console.log(
-    //   "filteredItem",
-    //   filteredItem,
-    //   "filteredItem[0].quantity",
-    //   filteredItem[0].quantity
-    // );
     if (filteredItem[0].quantity > 1) {
       newCart.forEach((item) => {
         if (item.id === purchasedItem.id) {
@@ -161,31 +124,3 @@ export default function App() {
     </>
   );
 }
-
-function render() {
-  const $app = document.querySelector("#app");
-  $app.innerHTML = `
-    <h1>Puppies</h1>
-    <main>
-    <section>
-    <PlayersList></PlayersList>
-    </section>
-    <section id="selected">
-    <h2>Puppy Details</h2>
-    <PuppyDetails></PuppyDetails>
-    </section>
-    <section id='playerForm'>
-    <NewPlayerForm><NewPlayerForm>
-    </section>
-    
-    </main>
-  `;
-  $app.querySelector("PlayersList").replaceWith(PlayersList());
-}
-
-async function init() {
-  await getTeams();
-  render();
-}
-
-init();
